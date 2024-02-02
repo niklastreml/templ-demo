df03f73d4b18eae7cc741854c679d7fb2bb615bf
Niklas Treml <niklas.treml@telekom.de>
2024-02-02 12:42:59 +0100

    feat: projects

--- database/migrations/000001_create_project_table.up.sql
+++ database/migrations/000001_create_project_table.up.sql
@@ -1,4 +1,10 @@
 CREATE TABLE IF NOT EXISTS project(
     id serial primary key,
-    name varchar not null
+    name varchar not null,
+    -- in mCPU
+    cpu integer not null,
+    -- in bytes
+    memory integer not null,
+    -- in bytes
+    storage integer not null
 )
