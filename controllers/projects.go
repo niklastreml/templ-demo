4d26d21a6430f1da75b21733ee15bf1a741f8f3f
Niklas Treml <niklas.treml@telekom.de>
2024-02-02 16:05:37 +0100

    feat: order projects

--- controllers/projects.go
+++ controllers/projects.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"htmx-templ/database/sqlc"
 	"htmx-templ/services/project"
 	"htmx-templ/views"
 
@@ -17,6 +18,7 @@ func NewProjects(ps project.ProjectService) *ProjectController {
 
 func (p *ProjectController) Register(app *fiber.App) {
 	app.Get("/projects", p.Index)
+	app.Post("/projects", p.NewProject)
 }
 
 func (p *ProjectController) Index(c *fiber.Ctx) error {
@@ -26,4 +28,18 @@ func (p *ProjectController) Index(c *fiber.Ctx) error {
 	}
 
 	return views.Render(c, views.Projects(projects))
+}
+
+func (p *ProjectController) NewProject(c *fiber.Ctx) error {
+	var np sqlc.NewProjectParams
+	if err := c.BodyParser(&np); err != nil {
+		return err
+	}
+
+	proj, err := p.ps.OrderProject(c.Context(), np)
+	if err != nil {
+		return err
+	}
+
+	return views.Render(c, views.ProjectOrdered(proj))
 }
