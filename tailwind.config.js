878f2de56cc9ad104b91ef4793a567fcfd315693
Niklas Treml <niklas.treml@telekom.de>
2024-02-09 13:40:30 +0100

    chore: dead code

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,6 @@ export default {
     },
   },
   plugins: [
-    // plugin(function ({ addVariant }) {
-    //   throw new Error('test')
-    //   addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
-    //   addVariant('htmx-request', ['&:htmx-request', '.htmx-request &'])
-    //   addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
-    //   addVariant('htmx-added', ['&.htmx-added', '.htmx-added &'])
-    // }),
     require('tailwind-htmx'),
 
     require('flowbite/plugin')({
